23a524c2eab56eba6e65c41c4e90573b6b7826b1
Ivan C Myrvold <ivan@advantek.no>
2021-05-02 06:29:58 +0200

    Initial Commit

---
+++ Shared/RekClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RekClientApp.swift
+//  Shared
+//
+//  Created by Ivan C Myrvold on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct RekClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
